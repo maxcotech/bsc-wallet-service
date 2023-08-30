25a9dfd6cd42cae625df61dc836dafc3c158e003
Chisom Maxwell <chiscomax2013@gmail.com>
2023-08-30 12:09:14 +0100

    Add Fee Estimate Endpoint

--- src/controllers/TransactionController.ts
+++ src/controllers/TransactionController.ts
@@ -1,17 +1,25 @@
 import Controller from './Controller';
 import TransactionService from '../services/TransactionService';
 import { HttpRequestParams } from './../dataTypes/Http';
-import { VAULT_ADDRESS } from '../config/settings';
+import { DECIMAL_PLACES, VAULT_ADDRESS } from '../config/settings';
 import AppDataSource from '../config/dataSource';
 import Contract from '../entities/Contract';
+import { ethers } from 'ethers';
 
 
 export default class TransactionController extends Controller {
 
     public static async getFeeEstimate({ req }: HttpRequestParams) {
         const txnService = new TransactionService();
+        const feeUnits = await txnService.fetchFeeEstimate(req.query?.from as string, (req?.query?.contract === null) ? false : true)
+        const contractRepo = AppDataSource.getRepository(Contract);
+        const getContractDecimalPlaces = async () => {
+            const contract = await contractRepo.findOne({ where: { contractAddress: req.query.contract as string } })
+            return contract?.decimalPlaces;
+        }
         return {
-            fee: txnService.fetchFeeEstimate(req.query?.from, (req?.query?.contract === null) ? false : true),
+            feeUnits,
+            fee: ethers.utils.formatUnits(feeUnits as ethers.BigNumberish, (req?.query?.contract) ? await getContractDecimalPlaces() : DECIMAL_PLACES),
             amount: req.query?.amount,
             from: req.query?.from,
             to: req.query?.to
